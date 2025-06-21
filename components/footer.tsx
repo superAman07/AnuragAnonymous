f6cf9833c89ee3cdec57ab79bedbad7b8be16610
superAman07 <rivalaman123@gmail.com>
2025-06-21 14:28:32 +0530

    asndo

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg-[#0f0f0f] text-[#efefef] py-3 md:py-8 lg:py-8 md:pb-0 px-4 md:px-6">
-      <div className="max-w-4xl mx-auto text-center lg:pb-32 md:20 space-y-6 md:space-y-8">
+      <div className="max-w-4xl mx-auto text-center lg:pb-32 md:20 space-y-2 md:space-y-4">
         <div className="space-y-0 mb-10 md:mb-20 md:space-y-0">
           <h2 className="text-sm sm:text-2xl md:text-3xl lg:text-4xl font-bold">
             Anonymous AnuraG  <span className="px-1 md:px-3 lg:px-3">|</span> VFX Generalist
@@ -31,7 +31,7 @@ export default function Footer() {
           </button>
         </div> */}
 
-        <div className="flex justify-center items-center space-x-3 sm:space-x-4 md:space-x-5 lg:space-x-6 pb-20">
+        <div className="flex justify-center items-start space-x-3 sm:space-x-4 md:space-x-5 lg:space-x-6 pb-20 mt-1">
           <Link href="https://www.youtube.com/@AnonymousAnuraG/videos" target="_blank" className="hover:opacity-70 transition-opacity">
             <svg width="30" height="30" className="w-5 h-5 md:w-6 md:h-6 lg:w-6 lg:h-6"  viewBox="0 0 24 24" fill="currentColor">
               <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
