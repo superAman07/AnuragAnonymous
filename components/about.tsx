69da8e8846d951bd3f732765c7fb848b8f2abe32
superAman07 <rivalaman123@gmail.com>
2025-06-21 13:09:17 +0530

    adjusted the image radius and U

--- components/about.tsx
+++ components/about.tsx
@@ -11,7 +11,7 @@ const About = () => {
       <div className="mb-6 md:mb-0 transition-transform transform hover:scale-110">
         <NextImage
           alt="Anurag"
-          className="rounded-full border-2 border-white shadow-xl h-48 w-48 md:h-48 md:w-48"
+          className="rounded-full border-2 border-white shadow-xl h-32 w-32 md:h-48 md:w-48"
           src="/anurag.jpg"
           width={288}
           height={288}
